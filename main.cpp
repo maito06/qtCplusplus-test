13ca69c40f24f7d7eec86a5e75b3d96bb80843b4
LUCA <lucazelmans@yahoo.fr>
2016-02-10 17:35:35 +0100

    those changes are here to let the program know how old is the user.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    cout << "Hello world" << endl;
+    int age(0);
+
+    cout << "Hello world! How old are you?" << endl;
+    cin >> age;
+    cout << "You are " << age << " old!" << endl;
 
     return a.exec();
 }
